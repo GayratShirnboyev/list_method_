3b40774236373130c796f2877ae03fcacd757f44
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-19 10:59:05 +0500

    sdff7

--- bin/list_method07.dart
+++ bin/list_method07.dart
@@ -6,5 +6,19 @@
     Returns:
         int: return answer
 */
+int func(List list01) {
+  int a = 0;
+  int c = 0;
+  int x = list01.length - 1;
+  while (x >= a) {
+    if (list01[a] == 0) {
+      c++;
+    }
+    a++;
+  }
+  return c;
+}
 
-void main() {}
+void main() {
+  print(func([1, 0, 1, 0, 1, 0, 1, 0, 1, 0]));
+}
