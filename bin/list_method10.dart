9da20b7479f4f4e17bd4a7438e4b9e2564017f48
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-19 13:02:03 +0500

    sdff10

--- bin/list_method10.dart
+++ bin/list_method10.dart
@@ -6,5 +6,28 @@
     Returns:
         list: return answer
 */
+List func(List x) {
+  int d = 0;
+  int i = 0;
+  num a = 0;
+  num y = 0;
+  List n = [];
+  int l = x.length;
+  while (l > i) {
+    if (x[i] == 0) {
+      a++;
+    } else {
+      y++;
+    }
+    i++;
+  }
+  a.toString();
+  y.toString();
+  n.add(y);
+  n.add(a);
+  return n;
+}
 
-void main() {}
+void main() {
+  print(func([1, 0, 1, 0, 1, 0, 1, 0, 1]));
+}
